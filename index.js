75df85e71a57922c0fb8dc7597c3eebd0c22b638
Riley Slayden <slaydenriley@gmail.com>
2020-07-29 16:06:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-// Code your solution in this file!
-<<<<<<< HEAD
-const companyName = 'Scuber'
-let mostProfitableNeighborhood = 'Chelsea'
-let companyCeo = 'Susan Smith'
-=======
 let companyName = 'Scuber';
-const companyName = "Scruber";
-let mostProfitableNeighborhood = "Chelsea";
-let companyCeo = "Susan Smith";
->>>>>>> 54f4795718a927ce87462636271adf6c8a58ce82
